db8506eeb363e670a6f05128a412813c34f7481e
Vladimir <kartayev@gmail.com>
2020-04-23 16:32:29 +0300

    feat: main title is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'happy-clicker';
+  name: string;
+  toStart = false;
+  toHighScore = false;
+  scores = [];
+  clicks = 0;
+  highscore = 0;
+  seconds: number;
+  interval: number;
+  END_GAME = 0;
+  STOP_COUNTDOWN = -5;
+  STOP_CLICKS_COUNT = -1;
+  DEFAULT_GAME_START = 10;
+
+  mainTitle() {
+    if (this.toStart) {
+      if (this.clicks > 0 && this.seconds > this.STOP_CLICKS_COUNT) {
+        return 'Click It, Baby!!!';
+      } else if (this.clicks > 0 && this.seconds <= this.STOP_CLICKS_COUNT) {
+        if (this.clicks > this.highscore) {
+          return 'Your New HIGHSCORE IS...';
+        } else {
+          return 'Your Score Is...';
+        }
+      } else {
+        return `Ready to Start, ${this.name}?`;
+      }
+    } else {
+      return `Welcome to Happy Clicker!`;
+    }
+  }
 }
